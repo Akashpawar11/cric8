db545d3a2b7981359fd9dafce77c3213e880742c
Akash Pawar <akashpawar1103@gmail.com>
2022-09-13 17:54:02 +0530

    asia cup match data
    Signed-off-by: Akash Pawar <akashpawar1103@gmail.com>

---
+++ src/components/Header.vue
@@ -0,0 +1,119 @@
+<template>
+    <div>
+        <b-navbar class="navbar" type="dark" toggleable="lg">
+            <b-navbar-brand class="brand" href="/">CRICBUZZ
+                <!-- <div class="red-dot"></div> -->
+            </b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse" class="bbb"></b-navbar-toggle>
+
+            <b-collapse class="collapseNav collapse show" id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <router-link class="routerLink justify-content-left" to='/'>Home</router-link>
+                    <!-- <router-link class="routerLink" to='/about-us'>Mat</router-link>
+                    <router-link class="routerLink" to='/contact'></router-link> -->
+                </b-navbar-nav>
+            </b-collapse>
+
+        </b-navbar>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'HeaderNav'
+}
+</script>
+
+<style>
+* {
+    padding: 0;
+    margin: 0;
+    transition: 0.4s;
+}
+
+.brand {
+    padding: 5px 10px;
+    border-radius: 3px;
+    border: 2px solid white;
+    font-family: 'Montserrat';
+    font-weight: 700;
+    color:#ffffff;
+
+}
+.navbar-brand{
+    color: #ffffff;
+}
+
+.red-dot {
+    height: 10px;
+    width: 10px;
+    margin: 0px 1px 3px 3px;
+    border-radius: 50%;
+    background-color: rgb(253 0 0);
+}
+
+a.routerLink {
+    color: white;
+    font-weight: 700;
+    font-family: 'Montserrat', sans-serif;
+}
+
+.collapseNav {
+    border-left: 3px solid white;
+    padding-left: 1rem;
+    margin-left: 1rem;
+}
+
+.bbb {
+
+    margin: 10px 0;
+}
+
+.navbar {
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+    padding: 15px;
+    background-color: #222222;
+    /* border-bottom: 6px solid rgb(255, 183, 0); */
+}
+
+.collapseNav {
+    background-color: #222222;
+
+}
+
+nav ul {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 0 0 0;
+    padding: 0 10px;
+    list-style: circle;
+}
+
+nav ul a {
+    /* display: flex;
+    justify-content: center;
+    align-items: center; */
+    /* margin: 0 0 0 0; */
+
+    padding: 12px;
+    margin-left: 5px;
+    margin-right: 5px;
+}
+
+nav ul a:hover {
+    text-decoration: none;
+    background-color: rgb(239, 239, 239);
+    color: rgb(0, 0, 0);
+    border-radius: 4px;
+    /* display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 0 0 0;
+    padding: 0 10px; */
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,103 @@
+<template>
+    <div>
+        <HeaderNav />
+        <div class="container-fluid p-0">
+
+            <div class="row">
+                <h2 style="padding: 25px 0 5px 0;">{{this.seriesName}}</h2>
+                <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3 matchCard" v-for="match in data" :key="match.id">
+                    <div class="matchData mx-auto">
+
+                        <h5 class="matchDesc">{{match.matchInfo.matchDesc}}, {{match.matchInfo.matchFormat}}</h5>
+                        <div class="matchScore">
+
+                            <h4><b>{{match.matchInfo.team1.teamSName}}</b> {{match.matchScore.team1Score.inngs1.runs}} - {{match.matchScore.team1Score.inngs1.wickets}}</h4>
+                            <h4><b>{{match.matchInfo.team2.teamSName}}</b> {{match.matchScore.team2Score.inngs1.runs}} - {{match.matchScore.team2Score.inngs1.wickets}}</h4>
+                        </div>
+                        <h5 class="matchStatus">{{match.matchInfo.status}}</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import HeaderNav from '../components/Header.vue'
+export default {
+    name: 'HomePage',
+    components: {
+        HeaderNav
+    },
+    data() {
+        return {
+            seriesName: '',
+            score: '',
+            data: []
+        }
+    },
+    async mounted() {
+        let results = await axios.get(
+            'https://unofficial-cricbuzz.p.rapidapi.com/matches/list', {
+            headers: {
+                'X-RapidAPI-Key': '4019c68f6fmsh2280c1edd5d1458p1a4489jsnbb84be4d501a',
+                'X-RapidAPI-Host': 'unofficial-cricbuzz.p.rapidapi.com'
+            },
+            params: { matchState: 'recent' }
+        });
+        console.log(results)
+        this.score = results.data.typeMatches[0].seriesAdWrapper[0].seriesMatches.matches[0].matchScore.team1Score.inngs1
+        this.seriesName = results.data.typeMatches[0].seriesAdWrapper[0].seriesMatches.seriesName
+        this.data = results.data.typeMatches[0].seriesAdWrapper[0].seriesMatches.matches
+    }
+}
+</script>
+
+<style>
+
+
+.row {
+    margin: 0;
+}
+.matchData{
+    width: 100%;
+    height: 14rem;
+    border: 2px solid black;
+    border-radius: 16px;
+    margin: 15px 0;
+}
+.matchDesc{
+    background-color: #e7e7e7;
+    border-radius: 15px 15px 0px 0px;
+    padding: 8px;
+    border-bottom: 2px solid;
+    margin: 0;
+    font-size: 1.1rem;
+    height: 4rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.matchScore{
+    padding: 10px 0 6px 0;
+    border-top: 2px solid;
+    border-top: 2px solid;
+    border-bottom: 2px solid;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: burlywood;
+}
+.matchStatus{
+    font-size: 1.2rem;
+    padding: 19.5px;
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    /* margin: 0; */
+    background-color: #9bd9e5;
+    border-radius: 0 0 16px 16px;
+}
+</style>
